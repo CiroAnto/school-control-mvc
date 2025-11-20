917c77cbec1e62f21dc51978fdea49a72a65d279
Ciro <ciroanto555@gmail.com>
2025-11-20 09:04:18 -0600

    rutas subjects

--- src/routes/subjects.routes.js
+++ src/routes/subjects.routes.js
@@ -7,6 +7,6 @@ router.get("/getAll", subjectsControllers.getAll);
 router.get("/getOne/:subject_id", subjectsControllers.getOne);
 router.post("/insertOne", subjectsControllers.insertOne);
 router.post("/updateOne/:subject_id", subjectsControllers.updateOne);
-router.delete("/deleteOne/:subject_id", subjectsControllers.deleteOne);
+router.get("/deleteOne/:subject_id", subjectsControllers.deleteOne);
 
 export default router;
